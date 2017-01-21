5956e82f3cf08f5132cc489466fdfc09622cd119
Steve Marple <stevemarple@googlemail.com>
2017-01-21 17:19:29 +0000

    Include the zwoasi Camera object, not derive from it

--- cameralogger/zwo.py
+++ cameralogger/zwo.py
@@ -6,18 +6,18 @@ import time
 import zwoasi
 
 
-class Camera(zwoasi.Camera):
+class Camera(object):
     def __init__(self, config):
         num_cameras = zwoasi.get_num_cameras()
         if num_cameras == 0:
             raise Exception('no camera present')
-        super(Camera, self).__init__(0)
+        self.camera = zwoasi.Camera(0)
         self.config = config
         self.capture_image_lock = threading.Lock()
 
         # Initialise
-        controls = self.get_controls()
-        self.start_video_capture()
+        controls = self.camera.get_controls()
+        self.camera.start_video_capture()
 
         # Read all camera controls defined in the config file
         for c in controls:
@@ -28,16 +28,20 @@ class Camera(zwoasi.Camera):
                 control_type = getattr(zwoasi, 'ASI_' + c.upper())
                 logger.debug('set control value %s to %s', cl, value)
                 if value == 'auto':
-                    self.set_control_value(control_type, default_value, auto=True)
+                    self.camera.set_control_value(control_type, default_value, auto=True)
                 else:
                     # Cast value to same type as default_value
-                    self.set_control_value(control_type, type(default_value)(value), auto=False)
+                    self.camera.set_control_value(control_type, type(default_value)(value), auto=False)
 
         if self.config.has_option('camera', 'image_type'):
             image_type = self.config.get('camera', 'image_type')
             logger.debug('set image type to %s', image_type)
-            self.set_image_type(getattr(zwoasi, 'ASI_IMG_' + image_type.upper()))
+            self.camera.set_image_type(getattr(zwoasi, 'ASI_IMG_' + image_type.upper()))
         time.sleep(2)
+
+    def __del__(self):
+        self.camera.stop_video_capture()
+        self.camera.close()
 
     def capture_image(self):
         logger.debug('capture_image: acquiring lock')
@@ -46,7 +50,7 @@ class Camera(zwoasi.Camera):
                 logger.debug('capture_image: acquired lock')
                 t = time.time()
                 img_info = self.get_control_values()
-                img = Image.fromarray(self.capture_video_frame()[:, :, ::-1])  # Swap from BGR order
+                img = Image.fromarray(self.camera.capture_video_frame()[:, :, ::-1])  # Swap from BGR order
                 img_info['DateTime'] = time.strftime('%Y-%m-%d %H:%M:%S+00:00', time.gmtime(t))
 
                 # Take CPU temperature as system temperature
@@ -63,10 +67,10 @@ class Camera(zwoasi.Camera):
             raise Exception('could not acquire lock')
 
     def get_control_values(self):
-        controls = self.get_controls()
+        controls = self.camera.get_controls()
         r = {}
         for k in controls:
-            r[k] = self.get_control_value(controls[k]['ControlType'])[0]
+            r[k] = self.camera.get_control_value(controls[k]['ControlType'])[0]
 
         # Fix up certain keys
         r['Exposure_s'] = r['Exposure'] / 1000000.0
