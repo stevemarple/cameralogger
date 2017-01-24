6efbf54d453a8cc641d8e06660c7a22284fe12f7
Steve Marple <stevemarple@googlemail.com>
2017-01-24 22:06:12 +0000

    Refactor LatLon class
    Use DegMS class to represent latitude and longitude, with optional
    conversion to minutes and seconds. Overload __format__ so that the class
    can be formatted as if it were the floating-point value of the latitude
    or longitude.

--- cameralogger/formatters.py
+++ cameralogger/formatters.py
@@ -1,5 +1,6 @@
 from string import Formatter
 
+
 class MyFormatter(Formatter):
     def __init__(self):
         Formatter.__init__(self)
@@ -28,75 +29,63 @@ class MyFormatter(Formatter):
 
 
 class LatLon(object):
-    def __init__(self, lat, lon):
-        self._lat = lat
-        self._lon = lon
-
-    def _dmsh(self, val, hemi):
-        is_positive = val >= 0
-
-        val = abs(val)
-        m, s = divmod(val * 3600, 60)
-        d, m = divmod(m, 60)
-        d = d if is_positive else -d
-        return int(d), int(m), s, self._hemisphere(val, hemi)
-
-    def _hemisphere(self, val, hemi):
-        if val > 0:
-           return hemi[0]
-        elif val < 0:
-            return hemi[1]
-        else:
-            return ''
+    def __init__(self, latitude, longitude):
+        self._latitude = DegMS(latitude, ('N', 'S'))
+        self._longitude = DegMS(longitude, ('E', 'W'))
 
     @property
     def latitude(self):
-        return abs(self._lat)
-
-    @property
-    def latitude_signed(self):
-        return self._lat
-
-    @property
-    def latitude_degrees(self):
-        return int(abs(self._lat))
-
-    @property
-    def latitude_minutes(self):
-        return self._dmsh(self._lat, ('N', 'S'))[1]
-
-    @property
-    def latitude_seconds(self):
-        return self._dmsh(self._lat, ('N', 'S'))[2]
-
-    @property
-    def latitude_hemisphere(self):
-        return self._hemisphere(self._lat, ('N', 'S'))
+        return self._latitude
 
     @property
     def longitude(self):
-        return abs(self._lon)
+        return self._longitude
+
+
+class DegMS(object):
+    def __init__(self, val, hemi):
+        self._val = val
+        self._hemi = tuple(hemi)
+
+    def __format__(self, *args, **kwargs):
+        """Format the class as if it was a floating point value."""
+        return self._val.__format__(*args, **kwargs)
+
+    def _hemisphere(self):
+        if self._val > 0:
+            return self._hemi[0]
+        elif self._val < 0:
+            return self._hemi[1]
+        else:
+            return ''
+
+    def _dmsh(self):
+        m, s = divmod(abs(self._val) * 3600, 60)
+        d, m = divmod(m, 60)
+        # d = d if self._val >= 0 else -d
+        return int(d), int(m), s, self._hemisphere()
 
     @property
-    def longitude_signed(self):
-        return self._lon
+    def degrees(self):
+        return abs(self._val)
 
     @property
-    def longitude_degrees(self):
-        return int(abs(self._lon))
+    def signed_degrees(self):
+        return self._val
 
     @property
-    def longitude_minutes(self):
-        return self._dmsh(self._lon, ('E', 'W'))[1]
+    def minutes(self):
+        return self._dmsh()[1]
 
     @property
-    def longitude_seconds(self):
-        return self._dmsh(self._lon, ('E', 'W'))[2]
+    def seconds(self):
+        return self._dmsh()[2]
 
     @property
-    def longitude_hemisphere(self):
-        return self._hemisphere(self._lon, ('E', 'W'))
+    def hemisphere(self):
+        return self._hemisphere()
 
-
-
+    @property
+    def dhms(self):
+        return self._dmsh()
 
