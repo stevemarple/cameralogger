7929a5f39b1f4ec8f1649d7935eb2fb54097bdcc
Steve Marple <stevemarple@googlemail.com>
2017-01-29 12:53:01 +0000

    Add program to make timelapse movies
    Wrapper for ffmpeg. Finds images by time. Pads gaps by repeating
    previous image so can work around varying cadence.

---
+++ cameralogger/bin/timelapse.py
@@ -0,0 +1,222 @@
+#!/usr/bin/env python
+
+import argparse
+import datetime
+import logging
+import numpy as np
+import os
+from PIL import Image
+import subprocess
+
+
+__author__ = 'Steve Marple'
+__version__ = '0.0.6'
+__license__ = 'PSF'
+
+
+def find_start_end_frames(st, et, step, filename_fstr):
+    r_st = r_et = None
+    t = st
+    while t < et:
+        dt = datetime.datetime.fromtimestamp(t)
+        filename = filename_fstr.format(DateTime=dt)
+        if os.path.exists(filename):
+            r_st = t
+            break
+        t += step
+    if r_st is None:
+        return None, None
+
+    t = et
+    while t >= r_st:
+        dt = datetime.datetime.fromtimestamp(t)
+        filename = filename_fstr.format(DateTime=dt)
+        if os.path.exists(filename):
+            r_et = t
+            break
+        t -= step
+
+    return r_st, r_et
+
+
+def ffmpeg(start_time, end_time, step, filename_fstr, output_filename,
+           resolution=None, duration=None, ifr=None, ofr=None):
+    img_mode = 'RGB'  # Mode for all images
+    # Remember et is inclusive
+    st, et = find_start_end_frames(start_time.astype('datetime64[s]').astype(int),
+                                   end_time.astype('datetime64[s]').astype(int),
+                                   step,
+                                   filename_fstr)
+    if st is None:
+        raise ValueError('could not find any images in time range')
+
+    frames = (et - st) / step  # Includes repeated frames
+    if ifr is None:
+        if duration is not None:
+            ifr = round(float(frames) / duration)
+            print(frames)
+            print(duration)
+            print(ifr)
+        else:
+            ifr = 1
+
+    if ofr is None:
+        ofr = 60
+        # Can a lower sensible rate be used, but one which is still larger than ifr?
+        for fr in (5, 10, 15, 20, 30, 60):
+            if fr >= ifr:
+                ofr = fr
+                break
+
+    first_frame = Image.open(filename_fstr.format(DateTime=datetime.datetime.fromtimestamp(st))).convert('RGB')
+
+    # TODO: Add code to run tasks at start/end of time lapse, eg add text, dissolving to/from first/last frames
+    if resolution is None:
+        resolution = tuple(first_frame.size)
+
+    # Set up a subprocess to run ffmpeg
+    cmd = ('ffmpeg',
+           '-loglevel', 'error',
+           '-y',  # overwrite
+           '-framerate', str(ifr),  # input frame rate
+           '-s', '%dx%d' % (resolution[0], resolution[1]),  # size of image string
+           '-pix_fmt', 'rgb24',  # format
+           '-f', 'rawvideo', '-i', '-',  # tell ffmpeg to expect raw video from the pipe
+           '-vcodec', 'libx264',  # output encoding
+           '-r', str(ofr),  # output frame rate
+           output_filename)
+    logger.info('Running command ' + ' '.join(cmd))
+    proc = subprocess.Popen(cmd, stdin=subprocess.PIPE)
+
+    img = None
+    t = st
+    while t <= et:
+        dt = datetime.datetime.fromtimestamp(t)
+        filename = filename_fstr.format(DateTime=dt)
+        if os.path.exists(filename):
+            logger.debug('reading %s', filename)
+            img = Image.open(filename)
+        elif img is None:
+            logger.debug('missing %s', filename)
+            img = Image.new('RGB', resolution)
+        else:
+            logger.debug('missing intermediate image %s', filename)
+
+        if img.mode != img_mode:
+            logger.debug('converting image to mode %s', img_mode)
+            img = img.convert(img_mode)
+        if tuple(img.size) != resolution:
+            logger.debug('resizing image')
+            img = img.resize(resolution, Image.BILINEAR)
+
+        proc.stdin.write(img.tobytes())
+        t += step
+    proc.communicate()
+    logger.info('saved to %s', output_filename)
+
+
+logger = logging.getLogger(__name__)
+if __name__ == '__main__':
+    default_config_file = os.path.join(os.path.sep, 'etc', 'camera.ini')
+    parser = \
+        argparse.ArgumentParser(description='')
+
+    parser.add_argument('-c', '--config-file',
+                        default=default_config_file,
+                        help='Configuration file')
+    parser.add_argument('-d', '--duration',
+                        type=float,
+                        help='Duration',
+                        metavar='SECONDS')
+    parser.add_argument('-e', '--end-time',
+                        required=True,
+                        help='End time for time lapse')
+    parser.add_argument('-f', '--fstr',
+                        required=True,
+                        help='Filenames format string')
+    parser.add_argument('--log-level',
+                        choices=['debug', 'info', 'warning',
+                                 'error', 'critical'],
+                        default='info',
+                        help='Control how much detail is printed',
+                        metavar='LEVEL')
+    parser.add_argument('-o', '--output',
+                        required=True,
+                        help='Output filename')
+    parser.add_argument('-s', '--start-time',
+                        required=True,
+                        help='Start time for time lapse')
+    parser.add_argument('--step',
+                        default=1,
+                        type=int,  # Could this be float?
+                        help='Time step',
+                        metavar='SECONDS')
+    args = parser.parse_args()
+
+    logging.basicConfig(level=getattr(logging, args.log_level.upper()))
+
+    start_time = np.datetime64(args.start_time, 's')
+    end_time = np.datetime64(args.end_time, 's')
+
+    ffmpeg(start_time, end_time, args.step, args.fstr, args.output, duration=args.duration)
+
+
+#
+#     # Find the first and last images
+# #start_time = np.datetime64('2017-01-26T20:00:08Z')
+# #end_time = np.datetime64('2017-01-26T21:00:00Z')
+# start_time = np.datetime64('2017-01-26T19:22:32Z')
+# end_time = np.datetime64('2017-01-26T22:00:00Z')
+# step = 1
+# filename_fstr = '/home/marple/picam/{DateTime:%Y%m%dT%H%M%S}.jpg'
+#
+# output_filename = '/tmp/movie.mp4'
+#
+#
+# if st is None:
+#     raise Exception('no images found')
+#
+#
+# proc = None
+# t = st
+# first_frame = True
+#
+# while t <= et:
+#     dt = datetime.datetime.fromtimestamp(t)
+#     filename = filename_fstr.format(DateTime=dt)
+#     if os.path.exists(filename):
+#         logger.info('reading %s', filename)
+#         img = Image.open(filename).convert('RGB')
+#     elif first_frame:
+#         logger.debug('missing %s', filename)
+#         t += step
+#         continue
+#     else:
+#         logger.debug('missing intermediate image %s', filename)
+#         # img = Image.new('RGB', img_size)
+#
+#     if first_frame:
+#         first_frame = False
+#         img_size = img.size
+#         cmd = ('ffmpeg',
+#                '-loglevel', 'error',
+#                '-y',  # overwrite
+#                '-framerate', '329',  # input frame rate
+#                '-s', '%dx%d' % (img_size[0], img_size[1]),  # size of image string
+#                '-pix_fmt', 'rgb24',  # format
+#                '-f', 'rawvideo', '-i', '-',  # tell ffmpeg to expect raw video from the pipe
+#                '-vcodec', 'libx264', # output encoding
+#                '-r', '30',  # output frame rate
+#                output_filename)
+#         proc = subprocess.Popen(cmd, stdin=subprocess.PIPE)
+#
+#     proc.stdin.write(img.tobytes())
+#     t += step
+#
+#
+#
+# if proc:
+#     proc.communicate()
+# else:
+#     raise Exception('no images found')
+#
